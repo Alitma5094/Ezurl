cc52ccaa850c59c94e23c7a64423bf089e408295
Andrew Litman <litmanandrew@gmail.com>
2023-12-31 14:09:02 -0500

    Add a page asking if users want to be redirected

--- shorts/views.py
+++ shorts/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, redirect
-from .models import Short
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render
+
+from .models import Short
 
 
 # Create your views here.
 
+
 def redirect_url(request, back_half):
     try:
         requested_short = Short.objects.get(back_half=back_half)
-        return redirect(requested_short.redirect_url)
+        return render(
+            request, "shorts/redirect.html", context={"redirect_url": requested_short}
+        )
     except ObjectDoesNotExist:
         return render(request, "shorts/404.html", status=404)
